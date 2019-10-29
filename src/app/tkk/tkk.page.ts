8b93ed0cfe32dad63bd89f7b02021fec056622d7
dhimasrpriyambodo <dhimasrizky@student.ppns.ac.id>
2019-10-29 11:38:33 +0700

    lutfi

---
+++ src/app/tkk/tkk.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tkk',
+  templateUrl: 'tkk.page.html',
+  styleUrls: ['tkk.page.scss']
+})
+export class TkkPage {
+
+  constructor() {}
+
+}
